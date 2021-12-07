aa13b99b7d129c40bba084e9086d5722a36ecde1
Hankyyh <yunhany@hotmail.com>
2021-12-07 15:14:10 +0800

    1019

---
+++ monotonic_stack/503#Next_Greater_Element_II.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        st = []
+        n = len(nums)
+        ans = [-1] * n
+        for i in range(2*n-1, -1, -1):
+            while st and st[-1] <= nums[i%n]:
+                st.pop()
+            if st and i < n:
+                ans[i] = st[-1]
+            st.append(nums[i%n])
+        return ans
