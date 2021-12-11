e761e26f3a59025dc28db02e1761a9d8f5dee43d
Hankyyh <yunhany@hotmail.com>
2021-12-11 17:08:20 +0800

    164

---
+++ sort/164#Maximum_Gap.py
@@ -0,0 +1,37 @@
+# Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
+#
+# You must write an algorithm that runs in linear time and uses linear extra space.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [3,6,9,1]
+# Output: 3
+# Explanation: The sorted form of the array is [1,3,6,9], either (3,6) or (6,9) has the maximum difference 3.
+# Example 2:
+#
+# Input: nums = [10]
+# Output: 0
+# Explanation: The array contains less than 2 elements, therefore return 0.
+
+
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        mi, ma, n = min(nums), max(nums), len(nums)
+        if mi == ma: return 0  # All elements are the same
+        bucketSize = math.ceil((ma - mi) / (n - 1))
+        minBucket = [math.inf] * n
+        maxBucket = [-math.inf] * n
+        for x in nums:
+            idx = (x - mi) // bucketSize
+            minBucket[idx] = min(minBucket[idx], x)
+            maxBucket[idx] = max(maxBucket[idx], x)
+
+        maxGap = bucketSize  # Maximum gap is always greater or equal to bucketSize
+        prev = maxBucket[0]  # We always have 0th bucket
+        for i in range(1, n):
+            if minBucket[i] == math.inf: continue  # Skip empty bucket
+            maxGap = max(maxGap, minBucket[i] - prev)
+            prev = maxBucket[i]
+        return maxGap
